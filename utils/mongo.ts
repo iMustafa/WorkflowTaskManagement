d1c9b3ed0f36df53e0daead89cd626e2c93a7bce
iMustafa <imustafa97@outlook.com>
2021-07-14 18:32:58 +0200

    @feature/server-setup Connect to MongoDB

---
+++ utils/mongo.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+require('dotenv').config();
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+const init = async () => {
+  try {
+    await mongoose.connect(
+      process.env.MONGO_URL,
+      options
+    )
+    console.log('>> Connected to MongoDB')
+  } catch (e) {
+    console.log(`Error while connecting to MongoDB: ${e}`);
+    return process.exit(0)
+  }
+};
+
+export default {
+  init,
+};
